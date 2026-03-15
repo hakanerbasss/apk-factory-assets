2b4a9c57bcf4ec4c0aedb4d9bc09a5f7779d2b69
hakanerbasss <hakan@apkfactory.local>
2026-03-15 22:47:04 +0300

    güncelleme 15.03.2026 22:47

--- scripts/factory.sh
+++ scripts/factory.sh
@@ -83,7 +83,17 @@ package $P_PKG
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
